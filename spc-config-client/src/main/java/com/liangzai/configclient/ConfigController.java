7a12c2fd19b71a4204c364aad875305c179fbbd0
zhanggaoliang <zhanggaoliang@rong360.com>
2019-10-16 15:46:02 +0800

    config

--- spc-config-client/src/main/java/com/liangzai/configclient/ConfigController.java
+++ spc-config-client/src/main/java/com/liangzai/configclient/ConfigController.java
@@ -1,13 +1,15 @@
 package com.liangzai.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
-    @Value("${config.client.message}")
+    @Value("${config.message}")
     String message;
 
     @RequestMapping(value = "/message")
--- spc-config-server/src/main/java/com/liangzai/configserver/SpcConfigServerApplication.java
+++ spc-config-server/src/main/java/com/liangzai/configserver/SpcConfigServerApplication.java
@@ -12,6 +12,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableDiscoveryClient
 public class SpcConfigServerApplication {
 
+    /**
+     * 配置文件名：config-dev.properties,config-pro.properties
+     * http://localhost:8070/config/dev/master 或 http://localhost:8070/config-dev.properties
+     * http://localhost:8070/config/test/master
+     * http://localhost:8070/config/pro/master
+     *
+     * http://localhost:8070/config.client.message/dev
+     *
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpcConfigServerApplication.class, args);
     }
