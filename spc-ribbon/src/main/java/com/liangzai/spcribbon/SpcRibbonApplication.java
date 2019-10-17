dcb828a421f79836a0885e07e51875b10170913d
zhanggaoliang <zhanggaoliang@rong360.com>
2019-10-17 15:07:11 +0800

    EnableHystrixDashboard

--- spc-ribbon/src/main/java/com/liangzai/spcribbon/SpcRibbonApplication.java
+++ spc-ribbon/src/main/java/com/liangzai/spcribbon/SpcRibbonApplication.java
@@ -1,11 +1,14 @@
 package com.liangzai.spcribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 // 熔断
 @EnableHystrix
+@EnableHystrixDashboard
 @ComponentScan(basePackages = "com.liangzai")
 public class SpcRibbonApplication {
 
@@ -26,6 +30,36 @@ public class SpcRibbonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpcRibbonApplication.class, args);
+    }
+
+
+    /**
+     * HystrixDashboard
+     * http://localhost:8091/hystrix
+     * http://localhost:8091/hystrix.stream
+     *
+     * 0.访问：
+     * http://localhost:8091/hystrix
+     *
+     * 1.依次输入：
+     * http://localhost:8091/hystrix.stream
+     * 2000（Delay:ms）
+     * miya（title）
+     *
+     * 2.访问：
+     * http://localhost:8091
+     *
+     * 3.刷新步骤0页面
+     *
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
